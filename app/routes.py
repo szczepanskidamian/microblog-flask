096d7e152f0c161f38a042052317d99775871e59
szczepanskidamian <bezpan96@gmail.com>
2019-04-16 19:39:49 +0200

    Added pagination and navigation

--- app/routes.py
+++ app/routes.py
@@ -18,8 +18,16 @@ def index():
         db.session.commit()
         flash('Succesfully added a post!')
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title="Home", form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title="Home", form=form, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/explore', methods=['GET'])
@@ -32,8 +40,16 @@ def explore():
         db.session.commit()
         flash('Succesfully added a post!')
         return redirect(url_for('explore'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title="Home", form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title="Home", form=form, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -79,8 +95,16 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = Post.query.filter_by(user_id=user.id).order_by(Post.timestamp.desc()).all()
-    return render_template('user.html', user=user, posts=posts, title=username+'\'s Profile')
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.filter_by(user_id=user.id).order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items, title=username+'\'s Profile',
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.before_request
