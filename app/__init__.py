a5e3c2327db8e0b2569f17f4f6e5750a2495661f
szczepanskidamian <bezpan96@gmail.com>
2019-05-13 21:44:47 +0200

    Added polish translation with flask-babel functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -9,6 +9,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from config import Config
 from flask_mail import Mail
+from flask_babel import Babel, lazy_gettext as _l
 
 
 app = Flask(__name__)
@@ -17,9 +18,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 mail = Mail(app)
+babel = Babel(app)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -49,6 +53,11 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -16,3 +16,4 @@ class Config():
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['mikroblogerrors@gmail.com']
     POSTS_PER_PAGE = 3
+    LANGUAGES = ['en', 'pl']
