10e4dae7c722ae3cd234da29aa37ddfc514a6434
szczepanskidamian <bezpan96@gmail.com>
2019-05-20 15:55:08 +0200

    Added notification badge for messages

--- app/__init__.py
+++ app/__init__.py
@@ -57,7 +57,7 @@ if not app.debug:
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-    # return 'en'
+    # return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'en'
 
 from app import routes, models, errors
